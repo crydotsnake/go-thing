354ff230054351ea9698e780baab570af36ca78a
Simon Krull <simondotunix@gmail.com>
2019-12-14 12:36:50 +0100

    Update Code and some other stuff ;)

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Go Thing
-A project that is written in the GO programming language, but i dont know what it will be. 
+A project that is written in the GO programming language, but i dont know what it will be.
 
 # Important
 
 This is just for learning!, so dont wondering if you want any mistakes! :blush:
+
+
+# Credits
+
+Thumbnail image was created on: [place-hold.it](https://place-hold.it/)
--- index.go
+++ index.go
@@ -1,7 +1,25 @@
-package main /* Importiert das main packet */
+package main
 
-import "fmt"
+import (
+    "image"
+    "image/png"
+    "os"
+)
 
 func main() {
-	fmt.Println("Hello, this is my GO Project")
+    // Create a blank image 100x200 pixels
+    myImage := image.NewRGBA(image.Rect(0, 0, 100, 200))
+
+    // outputFile is a File type which satisfies Writer interface
+    outputFile, err := os.Create("test.png")
+    if err != nil {
+    	// Handle error
+    }
+
+    // Encode takes a writer interface and an image interface
+    // We pass it the File and the RGBA
+    png.Encode(outputFile, myImage)
+
+    // Don't forget to close files
+    outputFile.Close()
 }
