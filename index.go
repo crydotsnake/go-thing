f1ca1313c62215ea1e584d16bbf55ddf947deeec
Simon Krull <simondotunix@gmail.com>
2019-12-16 16:32:28 +0100

    Update code documentation

--- index.go
+++ index.go
@@ -7,17 +7,22 @@ import (
 )
 
 func main() {
-    // Erstellt ein leeres Bild mit den maßen 100x200 pixeln
+
+    // Creates an empty image in size of 100x200
+    
     myImage := image.NewRGBA(image.Rect(0, 0, 100, 200))
 
     // outputFile is a File type which satisfies Writer interface
+    
     outputFile, err := os.Create("test.png")
     if err != nil {
     	// Wenn ein fehler im programm vorhanden ist, wird er ausgegeben.
     }
 
     png.Encode(outputFile, myImage)
-
-    // Schließt die Dateien.
+    
+    
+    // Close the files.
+    
     outputFile.Close()
 }
