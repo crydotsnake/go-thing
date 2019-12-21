8a373ff57c7c50efba0590c94aafaa8b170178a8
Simon Krull <simondotunix@gmail.com>
2019-12-21 23:47:08 +0100

    Delete Image & Update Code

--- index.go
+++ index.go
@@ -1,27 +1,9 @@
 package main
 
-import (
-	"image"
-	"image/png"
-	"os"
-)
+import "fmt"
 
 func main() {
 
-	// Creates an empty image in size of 100x200
-
-	myImage := image.NewRGBA(image.Rect(0, 0, 100, 200))
-
-	// outputFile is a File type which satisfies Writer interface
-
-	outputFile, err := os.Create("test.png")
-	if err != nil {
-		// If theres an error,there will be an output.
-	}
-
-	png.Encode(outputFile, myImage)
-
-	// Close the files.
-
-	outputFile.Close()
+	fmt.Println("Hello, from Golang!.")
+	
 }
