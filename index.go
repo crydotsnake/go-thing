b6aff26f12a137c346bef620f9d4acffc14e55c9
Simon Krull <simondotunix@gmail.com>
2019-12-21 08:38:50 +0100

    Update index.go

--- index.go
+++ index.go
@@ -1,28 +1,27 @@
 package main
 
 import (
-    "image"
-    "image/png"
-    "os"
+	"image"
+	"image/png"
+	"os"
 )
 
 func main() {
 
-    // Creates an empty image in size of 100x200
-    
-    myImage := image.NewRGBA(image.Rect(0, 0, 100, 200))
+	// Creates an empty image in size of 100x200
 
-    // outputFile is a File type which satisfies Writer interface
-    
-    outputFile, err := os.Create("test.png")
-    if err != nil {
-    	// If theres an error,there will be an output.
-    }
+	myImage := image.NewRGBA(image.Rect(0, 0, 100, 200))
 
-    png.Encode(outputFile, myImage)
-    
-    
-    // Close the files.
-    
-    outputFile.Close()
+	// outputFile is a File type which satisfies Writer interface
+
+	outputFile, err := os.Create("test.png")
+	if err != nil {
+		// If theres an error,there will be an output.
+	}
+
+	png.Encode(outputFile, myImage)
+
+	// Close the files.
+
+	outputFile.Close()
 }
